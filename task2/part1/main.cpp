eefc81ecbe77891d8f21a144c5c6a13aecd17c0a
buklinfur <buklinfur@outlook.com>
2025-03-31 18:41:10 +0000

    added task2.1

---
+++ task2/part1/main.cpp
@@ -0,0 +1,66 @@
+#include <omp.h>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#ifdef BIG
+#define msize 40000
+#else
+#define msize 20000
+#endif
+
+using namespace std;
+
+int main() {
+
+	cout << msize << std::endl;
+	
+	vector<vector<double> > A(msize);
+
+	for (int i = 0; i < msize; i++) {
+		A[i].resize(msize);
+		for (int j = 0; j < msize; j++) {
+			A[i][j] = i+j;
+		}
+	}
+
+	vector<double> b(msize);
+
+	for (int i = 0; i < msize; i++)
+		b[i] = 5 * i;
+	vector<double> c(msize);
+
+	int potoks[] = {1, 2, 4, 7, 8, 16, 20, 40};
+
+    
+	for (int i = 0; i < 8; i++) {
+		int thread_amount = potoks[i];
+		cout << "code started\n";
+		auto begin = chrono::steady_clock::now();
+
+		#pragma omp parallel num_threads(thread_amount)
+		{
+			int threadid = omp_get_thread_num();
+			int items_per_thread = msize / thread_amount;
+			int left_bound = threadid * items_per_thread;
+			int right_bound;
+			if (threadid == thread_amount - 1) right_bound = msize - 1;
+			else right_bound = left_bound + items_per_thread;
+			for (int i = left_bound; i < right_bound; i++) {
+				for (int j = 0; j < msize; j++)
+					c[i] += A[i][j] * b[j];
+			}
+		}
+
+		auto end = chrono::steady_clock::now();
+		auto elapsed_ms = chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+		cout << potoks[i] << " threads " << elapsed_ms.count()/1000.0 << " seconds\n";
+		
+		#pragma omp barrier
+		{
+			cout << "test element " << c.at(10000) << std::endl;
+		}
+	}
+	
+	return 0;
+}
